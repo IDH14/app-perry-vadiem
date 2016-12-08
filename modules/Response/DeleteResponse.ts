15f291e4133049c10376fa02b4ea8ab199fc2cf2
Vadiem Janssens <vadiem.janssens@webcolorsgroup.com>
2016-12-08 16:48:48 +0100

    Added DeleteResponse implementation

--- modules/Response/DeleteResponse.ts
+++ modules/Response/DeleteResponse.ts
@@ -1,14 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const dir = __dirname;
+import { checksum } from './../Helpers';
+const fileDir = path.join(dir + '/../../', '/server-files');
+
 import Response from './Response';
 import ResponseInterface from './ResponseInterface';
 import Request from './../Request';
 export default class DeleteResponse extends Response implements ResponseInterface {
 
     create() {
-        
-        this.object = {
-            status: '404',
-            message: 'Method not supported, see protocol: https://idh14.github.io/protocol/'
-        };
-    }
+        try {
+            const filename = this.request.body['filename'];
+            const filePath = path.join(fileDir, filename);
+            fs.unlinkSync(filePath);
 
+            this.object = { 'status': 200 };
+        } catch (error) {
+            this.object = { 'status': 100 };
+        }
+    }
 }
