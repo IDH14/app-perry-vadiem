fd8366299b1a8e24a6e7276eabb9f28dddc496db
Vadiem Janssens <vadiem.janssens@webcolorsgroup.com>
2016-12-11 16:10:20 +0100

    Added base64 helper methods

--- src/modules/Helpers.ts
+++ src/modules/Helpers.ts
@@ -1,5 +1,13 @@
 import crypto = require('crypto');
 
+export function toBase64(str: string) {
+    return Buffer.from(str).toString('base64')
+}
+
+export function fromBase64(str: string) {
+   return  Buffer.from(str, 'base64').toString('ascii');
+}
+
 export function checksum(str, algorithm = 'sha1', encoding?) {
     return crypto
         .createHash(algorithm)
